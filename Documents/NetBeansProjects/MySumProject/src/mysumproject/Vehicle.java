036e2167722533fc41085ca471967935c19b42bc
Melaku Girma <melakugrm@gmail.com>
2019-01-23 21:28:58 +0000

    Add gallonsOfGas and mpg + getters and setters

--- Documents/NetBeansProjects/MySumProject/src/mysumproject/Vehicle.java
+++ Documents/NetBeansProjects/MySumProject/src/mysumproject/Vehicle.java
@@ -10,5 +10,34 @@ package mysumproject;
  * @author Melaku
  */
 public class Vehicle {
-    
+    private double gallonOfGas;
+    private int mpg;
+
+    /**
+     * @return the gallonOfGas
+     */
+    public double getGallonOfGas() {
+        return gallonOfGas;
+    }
+
+    /**
+     * @param gallonOfGas the gallonOfGas to set
+     */
+    public void setGallonOfGas(double gallonOfGas) {
+        this.gallonOfGas = gallonOfGas;
+    }
+
+    /**
+     * @return the mpg
+     */
+    public int getMpg() {
+        return mpg;
+    }
+
+    /**
+     * @param mpg the mpg to set
+     */
+    public void setMpg(int mpg) {
+        this.mpg = mpg;
+    }
 }
